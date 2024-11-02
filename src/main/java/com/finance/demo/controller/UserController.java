0be6eabe3eac4debd53d97396a8809eab9bbc956
Alberto <albertojunior848423803@gmail.com>
2024-11-03 00:59:01 +0300

    changes

---
+++ src/main/java/com/finance/demo/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.finance.demo.controller;
+
+import com.finance.demo.dto.UserDTO;
+import com.finance.demo.model.User;
+import com.finance.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO){
+        User savedUser = userService.saveUser(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUser();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+}
