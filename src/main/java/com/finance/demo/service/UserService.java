3f6d86b682484af40e0a9944a517127428d2f099
Alberto <albertojunior848423803@gmail.com>
2024-11-03 00:33:49 +0300

    comitts

--- src/main/java/com/finance/demo/service/UserService.java
+++ src/main/java/com/finance/demo/service/UserService.java
@@ -2,20 +2,14 @@ package com.finance.demo.service;
 
 import com.finance.demo.dto.UserDTO;
 import com.finance.demo.model.User;
-import com.finance.demo.repository.UserRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    @Autowired private UserRepository userRepository;
-
     public User saveUser(UserDTO userDTO) {
 
         User user = new User();
-
-        return null;
     }
 }
