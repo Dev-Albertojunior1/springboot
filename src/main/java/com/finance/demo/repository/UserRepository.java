911d33a891a44292e6f51ef229a57b69401e2e15
Alberto <albertojunior848423803@gmail.com>
2024-11-02 10:57:27 +0300

    commits

--- src/main/java/com/finance/demo/repository/UserRepository.java
+++ src/main/java/com/finance/demo/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.finance.demo.repository;
 
 import com.finance.demo.model.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-}
+public interface UserRepository extends JpaRepository<User, Long> {}
+
