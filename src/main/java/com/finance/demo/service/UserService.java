20ce6d07fb4ea1e4c6a29060bf53c300e7c050ee
Alberto <albertojunior848423803@gmail.com>
2024-11-03 00:47:59 +0300

    commits

--- src/main/java/com/finance/demo/service/UserService.java
+++ src/main/java/com/finance/demo/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -23,5 +24,9 @@ public class UserService {
 
     public List<User> getAllUser(){
         return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id){
+        return userRepository.findById(id);
     }
 }
