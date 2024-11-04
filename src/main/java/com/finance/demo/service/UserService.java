18a000a88ad6035726e7cd73b9bdd743f8a0c7d5
Alberto <albertojunior848423803@gmail.com>
2024-11-04 20:35:15 +0300

    changes

--- src/main/java/com/finance/demo/service/UserService.java
+++ src/main/java/com/finance/demo/service/UserService.java
@@ -2,31 +2,23 @@ package com.finance.demo.service;
 
 import com.finance.demo.dto.UserDTO;
 import com.finance.demo.model.User;
-import com.finance.demo.repository.UserRepository;
 
+import com.finance.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
 
-    @Autowired private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     public User saveUser(UserDTO userDTO) {
         User user = new User();
         user.setName(user.getName());
         user.setEmail(user.getEmail());
+        
         return userRepository.save(user);
-    }
 
-    public List<User> getAllUser(){
-        return userRepository.findAll();
-    }
-
-    public Optional<User> getUserById(Long id){
-        return userRepository.findById(id);
     }
 }
